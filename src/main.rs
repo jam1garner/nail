a4bb7e23fa60ee8cf5e56b4bf6d6bfa3c392eacd
jam1garner <jam1.mcleod@hotmail.com>
2019-02-01 23:33:41 -0500

    Add simple mode swapping

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,38 @@
 mod util;
 mod file;
 mod modes;
+mod command_handler;
+mod app;
 
 use std::io;
+use std::io::Write;
 
+use termion::cursor::Goto;
 use termion::event::Key;
 use termion::input::MouseTerminal;
 use termion::raw::IntoRawMode;
 use termion::screen::AlternateScreen;
 use tui::backend::TermionBackend;
-use tui::layout::{Constraint, Direction, Layout};
+use tui::layout::{Constraint, Direction, Layout, Rect};
 use tui::style::{Color, Style};
-use tui::widgets::{Block, Borders, Tabs, Widget, Paragraph};
+use tui::widgets::{Block, Borders, Tabs, Text, Widget, Paragraph};
 use tui::Terminal;
 use crate::file::File;
 use crate::modes::Mode;
 
 use crate::util::event::{Event, Events};
 use crate::util::TabsState;
+use crate::app::{App, Term};
 
-struct App<'a> {
-    tabs: TabsState<'a>,
-    files: Vec<File<'a>>,
-    mode: Mode
-}
-
-fn default_mode(events: &Events, app: &mut App) -> Result<(), failure::Error>  {
+fn default_mode(events: &Events, app: &mut App, terminal: &mut Term) -> Result<(), failure::Error>  {
     match events.next()? {
         Event::Input(input) => match input {
             Key::Char('q') =>
                 app.mode = Mode::Quit,
-            Key::Char(':') =>
-               app.mode = Mode::Command,
+            Key::Char(':') => {
+               app.mode = Mode::Command;
+               app.command = String::from(":");
+            }
             Key::Char('i') =>
                 app.mode = Mode::Insert,
             Key::Right => app.tabs.next(),
@@ -44,10 +45,28 @@ fn default_mode(events: &Events, app: &mut App) -> Result<(), failure::Error>  {
     Ok(())
 }
 
-fn command_mode(events: &Events, app: &mut App) -> Result<(), failure::Error> {
+fn command_mode(events: &Events, app: &mut App, terminal: &mut Term) -> Result<(), failure::Error> {
+    // Move cursor to proper position
+    write!(
+        terminal.backend_mut(),
+        "{}",
+        Goto(1 + app.command.len() as u16, app.size.height)
+    )?;
+    // Command mode event handling
     match events.next()? {
         Event::Input(input) => match input {
             Key::Esc => app.mode = Mode::Default,
+            Key::Char('\n') => {
+                command_handler::handle_command(app, terminal);
+                app.mode = Mode::Default;
+                app.command = String::new();
+            } 
+            Key::Char(c) => app.command.push(c),
+            Key::Backspace => {
+                if app.command.pop().unwrap() == ':' && app.command.len() == 0 {
+                    app.mode = Mode::Default;
+                }
+            }
             _ => {}
         },
         _ => {}
@@ -55,7 +74,7 @@ fn command_mode(events: &Events, app: &mut App) -> Result<(), failure::Error> {
     Ok(())
 }
 
-fn insert_mode(events: &Events, app: &mut App) -> Result<(), failure::Error> {
+fn insert_mode(events: &Events, app: &mut App, terminal: &mut Term) -> Result<(), failure::Error> {
     match events.next()? {
         Event::Input(input) => match input {
             Key::Esc => app.mode = Mode::Default,
@@ -67,16 +86,7 @@ fn insert_mode(events: &Events, app: &mut App) -> Result<(), failure::Error> {
 } 
 
 fn main() -> Result<(), failure::Error> {
-    // Terminal initialization
-    let stdout = io::stdout().into_raw_mode()?;
-    let stdout = MouseTerminal::from(stdout);
-    let stdout = AlternateScreen::from(stdout);
-    let backend = TermionBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    terminal.hide_cursor()?;
-
-    let events = Events::new();
-    
+    // Hardcoded files atm
     let filenames = vec!["File 0", "File 1", "File 2","File 3"];
     let filepaths = vec!["C:/path/to/file/0.txt",
                         "C:/path/to/file/1.txt",
@@ -97,23 +107,38 @@ fn main() -> Result<(), failure::Error> {
             })
             .collect();
 
+    // Terminal initialization
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
     // App
     let mut app = App {
         files,
         tabs: TabsState::new(filenames),
-        mode: Mode::Default
+        mode: Mode::Default,
+        command: String::new(),
+        size: Rect::new(0,0,0,0)
     };
 
+    let events = Events::new();
+    
     // Main loop
     loop {
+        match app.mode {
+            Mode::Command => terminal.show_cursor()?,
+            _ => terminal.hide_cursor()?
+        }
+        
         terminal.draw(|mut f| {
-            let size = f.size();
-            let line_count = size.height as usize;
+            app.size = f.size();
+            let line_count = app.size.height as usize;
             let chunks = Layout::default()
                 .direction(Direction::Vertical)
                 //.margin(5)
                 .constraints([Constraint::Length(3), Constraint::Min(3), Constraint::Length(1)].as_ref())
-                .split(size);
+                .split(app.size);
 
             Block::default()
                 .style(Style::default().bg(
@@ -121,7 +146,7 @@ fn main() -> Result<(), failure::Error> {
                             Mode::Command => Color::Red,
                             _ => Color::Cyan
                         }))
-                .render(&mut f, size);
+                .render(&mut f, app.size);
             Tabs::default()
                 .block(Block::default().borders(Borders::ALL).title("Tabs"))
                 .titles(&app.tabs.titles)
@@ -146,12 +171,20 @@ fn main() -> Result<(), failure::Error> {
                 }
                 _ => {}
             }
+            Paragraph::new(vec![Text::raw(app.command.clone())].iter())
+                .style(Style::default().bg(
+                        match app.mode {
+                            Mode::Command => Color::Red,
+                            _ => Color::Cyan
+                        }))
+                .render(&mut f, chunks[2]);
         })?;
         
+        
         match app.mode {
-            Mode::Default => default_mode(&events, &mut app)?,
-            Mode::Command => command_mode(&events, &mut app)?,
-            Mode::Insert => insert_mode(&events, &mut app)?,
+            Mode::Default => default_mode(&events, &mut app, &mut terminal)?,
+            Mode::Command => command_mode(&events, &mut app, &mut terminal)?,
+            Mode::Insert => insert_mode(&events, &mut app, &mut terminal)?,
             Mode::Quit => break,
             _ => {}
         };
