d5a382e9e8357bb12ced7ecf7a9fbe99fa8eb6a4
jam1garner <jam1.mcleod@hotmail.com>
2019-02-04 20:31:44 -0500

    Add tab swapping commands

--- src/command_handler.rs
+++ src/command_handler.rs
@@ -6,38 +6,48 @@ pub fn handle_command(app: &mut App, terminal: &mut Term) {
     let mut force_command = false;
     let command = app.command.clone();
     let mut command_chars = command.chars();
-    match command_chars.next() {
-        Some(':') => {
-            for (index, c) in command_chars.enumerate() {
-                match c {
-                    'q' => {
-                        // NOTE: Unless forced, quit may be undone later
-                        app.mode = Mode::Quit;
-                    }
-                    'w' => {
-                        // TODO: Handle writing file
-                    }
-                    'a' => {
-                        // TODO: Handle marking all
-                    }
-                    '!' => {
-                        if index == 0 {
-                            app.mode = Mode::Bash;
-                            app.command = command[2..].to_string();
-                            return;
-                        } 
-                        else {
-                            force_command = true;
+    match command.as_ref() {
+        ":bnext" => {
+            app.tabs.next();
+        }
+        ":bprev" => {
+            app.tabs.previous();
+        }
+        _ => {
+            match command_chars.next() {
+                Some(':') => {
+                    for (index, c) in command_chars.enumerate() {
+                        match c {
+                            'q' => {
+                                // NOTE: Unless forced, quit may be undone later
+                                app.mode = Mode::Quit;
+                            }
+                            'w' => {
+                                // TODO: Handle writing file
+                            }
+                            'a' => {
+                                // TODO: Handle marking all
+                            }
+                            '!' => {
+                                if index == 0 {
+                                    app.mode = Mode::Bash;
+                                    app.command = command[2..].to_string();
+                                    return;
+                                }
+                                else {
+                                    force_command = true;
+                                }
+                            }
+                            _ => {}
                         }
                     }
-                    _ => {}
                 }
+                Some('/') => {
+
+                }
+                _ => {}
             }
         }
-        Some('/') => {
-
-        }
-        _ => {}
     }
     
     // TODO: Add checking if file needs to be saved + check for force quit
